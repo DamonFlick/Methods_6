eea51287e2be2033b1997a881964fdc6f013954c
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-24 15:57:39 -0800

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_6
+{
+   public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public  void SayName()
+        {
+            Console.WriteLine("Name :" + FirstName + LastName);
+        }
+    }
+}
